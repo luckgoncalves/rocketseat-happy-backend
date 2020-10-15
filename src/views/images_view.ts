a1ceb68a8999da9d22fba92068904e565cb10ce7
Lucas Goncalves <lucasgoncalves@Lucass-MacBook-Pro.local>
2020-10-15 00:43:52 -0300

    adding src

---
+++ src/views/images_view.ts
@@ -0,0 +1,13 @@
+import Image from '../models/Images'
+
+export default {
+  render(image: Image) {
+    return {
+      id: image.id,
+      ur: `http://localhost:3333/uploads/${image.path}`
+    };
+  },
+  renderMany(images: Image[]) {
+    return images.map(image => this.render(image))
+  }
+}
